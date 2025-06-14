bfbe7620cbf55ad7a5ea272eedb70df028c7502e
Aashvi-09 <aagajera96@gmail.com>
2025-06-14 22:13:13 +0530

    Update demux_1X2.v

--- DEMUX_1X2/demux_1X2.v
+++ DEMUX_1X2/demux_1X2.v
@@ -1,1 +1,10 @@
-DEMULTIPLEXER
+// Code your design here
+module demux_1X2_datafow(
+  input wire in,
+  input wire s,
+  output reg Q0,
+  output reg Q1
+);
+  assign Q0 = (~s & in);
+  assign Q1 = (s & in);
+endmodule
