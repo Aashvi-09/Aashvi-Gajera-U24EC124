152d5c0d19182e66610cd710bc74869d77d9611e
Aashvi-09 <aagajera96@gmail.com>
2025-06-14 22:16:43 +0530

    Update mux_2X1_dataflow.v

--- MUX_2X1/mux_2X1_dataflow.v
+++ MUX_2X1/mux_2X1_dataflow.v
@@ -1,1 +1,9 @@
-Multiplexer 2X1
+// Code your design here
+module mux_2X1_dataflow(
+  input wire i0,
+  input wire i1,
+  input wire s,
+  output wire Q
+);
+  assign Q = (~s & i0) | (s & i1);
+endmodule
